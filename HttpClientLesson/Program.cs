c6215c8313c7be1cb7f207973273100e04871d40
Matteo Sicilia <150261032+Matteo-Sicilia@users.noreply.github.com>
2024-02-12 21:20:15 +0100

    primo commit

---
+++ HttpClientLesson/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace HttpClientLesson
+{
+    public class PeopleResult
+    {
+        public List<Person> Results { get; set; }
+        public string Next { get; set; }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Height { get; set; }
+        public string Mass { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            decimal TotalWeight = 0;
+            decimal TotalHeight = 0;
+            decimal MedianWeight = 0;
+            decimal MedianHeight = 0;
+            decimal Counter = 0;
+
+            var client = new HttpClient();
+            string url = "https://swapi.dev/api/people";
+            while (url != null)
+            {
+                var response = await client.GetFromJsonAsync<PeopleResult>(url);
+
+                foreach (var person in response.Results)
+                {
+                    Counter++;
+                    if (person.Mass != "unknown")
+                    {
+                        decimal temp = Convert.ToDecimal(person.Mass);
+                        TotalWeight += temp;
+                    }
+                    if (person.Height != "unknown")
+                    {
+                        decimal temp = Convert.ToDecimal(person.Height);
+                        TotalHeight += temp;
+                    }
+                    Console.WriteLine($"{person.Name} altezza {person.Height} peso {person.Mass}");
+
+                }
+
+                url = response.Next;
+
+            }
+
+            MedianHeight = TotalHeight / Counter;
+            string HeightToString = String.Format("{0:0.##}", MedianHeight);
+            MedianWeight = TotalWeight / Counter;
+            string WeightToString = String.Format("{0:0.##}", MedianWeight);
+
+            Console.WriteLine();
+            Console.WriteLine($"Peso Medio: {WeightToString}");
+            Console.WriteLine($"Altezza Media: {HeightToString}");
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ HttpClientLesson/Swapi.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Net.Http.Json;
+using System.Text.Json;
+namespace HttpClientLesson;
+
+
+public class RootObject
+{
+    public int count { get; set; }
+    public string next { get; set; }
+    public object previous { get; set; }
+    public Results[] results { get; set; }
+}
+
+public class Results
+{
+    public string name { get; set; }
+    public string height { get; set; }
+    public string mass { get; set; }
+    public string hair_color { get; set; }
+    public string skin_color { get; set; }
+    public string eye_color { get; set; }
+    public string birth_year { get; set; }
+    public string gender { get; set; }
+    public string homeworld { get; set; }
+    public string[] films { get; set; }
+    public string[] species { get; set; }
+    public string[] vehicles { get; set; }
+    public string[] starships { get; set; }
+    public string created { get; set; }
+    public string edited { get; set; }
+    public string url { get; set; }
+}
+
